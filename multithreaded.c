0f829c9050e062b3d8f26a0259ab80eba011b694
SmurfNiZed <zipacaldo@up.edu.ph>
2023-01-10 20:13:59 +0800

    Singlethread 90% working, Multithread with no locks

--- multithreaded.c
+++ multithreaded.c
@@ -4,88 +4,227 @@
 #include <sys/types.h>
 #include <string.h>
 #include <pthread.h>
-#include <unistd.h>
-#include <sys/syscall.h>
+#include <semaphore.h>
 
 #define BUFFER 1000
 
-void *grepThread(char *path, char *search) 
+int count = 0;
+int workers = 0;
+int currentWorkers = 0;
+char* search_string;
+
+// Creating a linked list queue: https://stackoverflow.com/questions/36394860/linked-list-of-strings-in-c
+struct task
 {
+    char *dir;
+    struct task *next;
+};
+
+struct taskQueue 
+{
+    struct task *front, *rear;
+};
+
+struct task* newTask(char *dir)
+{
+    struct task* temp = (struct task*)malloc(sizeof(struct task));
+    temp->dir = dir;
+    temp->next = NULL;
+    return temp;
+}
+
+char* getAbsolutePath(char* path)
+{
+    char* actualPath;
+    actualPath = (char*)malloc(BUFFER);
+    char *ptr;
+    ptr = realpath(path, actualPath);           // Getting absolute path: https://stackoverflow.com/questions/229012/getting-absolute-path-of-a-file
+
+    return actualPath;
+}
+
+struct taskQueue* q;
+pthread_t* thread;
+// struct task* temp = newTask(getAbsolutePath(dir));
+
+
+// struct taskQueue* createQueue(char* dir)
+// {
+//     struct taskQueue* q = (struct taskQueue*)malloc(sizeof(struct taskQueue));
+//     struct task* temp = newTask(getAbsolutePath(dir));
+//     q->front = q->rear = temp;
+    
+//     return q;
+// }
+
+void enqueue(int id, char* dir)
+{
+    struct task* temp = newTask(dir);
+    printf("[%d] ENQUEUE %s\n", id, dir);
+
+    if(q->rear == NULL)
+    {
+        q->front = q->rear = temp;
+        return;
+    }
+    q->rear->next = temp;
+    q->rear = temp;
+}
+
+char* dequeue(int id)
+{
+    if (q->front == NULL)
+    {
+        return NULL;
+    }
+
+    struct task* temp = q->front;
+    char *dir = temp->dir;
+
+    q->front = q->front->next;
+
+    if(q->front == NULL)
+    {
+        q->rear = NULL;
+    }
+
+    printf("[%d] DIR %s\n", id, getAbsolutePath(dir));
+    free(temp);
+    return dir;
+}
+
+// int countElements(struct task* head) {
+//     int count = 0;
+
+//     while (head != NULL) {
+//         count++;
+//         head = head->next;
+//     }
+
+//     return count;
+    
+// }
+
+int parallelGrep(char* path)
+{
+
+}
+
+void *visit(int* arg)
+{
+    int id = *arg;
+
     DIR *dir;
     struct dirent *dp;
     char *file_name;
 
-    dir = opendir(path);                             // Traversing folders in C recursively: https://iq.opengenus.org/traversing-folders-in-c/
+    char *path = dequeue(id);
+    if(path == NULL)
+    {
+        return NULL;
+    }
+    count--;
+    dir = opendir(path);                          // Traversing folders in C recursively: https://iq.opengenus.org/traversing-folders-in-c/
 
     FILE *f;
-    char *filePath, *command;
+    char *command;
 
-    filePath = (char*)malloc(BUFFER);
     command = (char*)malloc(BUFFER);
 
-    while ((dp = readdir(dir)) != NULL)
+    while ((dp = readdir(dir)) != NULL)                 // File
     {
+        char* filePath;
+        filePath = (char*)malloc(BUFFER);
+        
         strcpy(filePath, path);                         // Concatenating strings: https://www.educative.io/blog/concatenate-string-c
         strcat(filePath, "/");
         strcat(filePath, dp->d_name);
 
-        char actualPath[BUFFER];
-        char *ptr;
-        ptr = realpath(filePath, actualPath);           // Getting absolute path: https://stackoverflow.com/questions/229012/getting-absolute-path-of-a-file
-
+        char* absolutePath;
+        absolutePath = getAbsolutePath(filePath);
 
         if (dp->d_type == DT_REG)                       // Checking file types: https://stackoverflow.com/questions/1121383/counting-the-number-of-files-in-a-directory-using-c
         {                                      
-            f = fopen(filePath, "r");                   // Accessing files: https://stackoverflow.com/questions/16869467/command-line-arguments-reading-a-file
+            f = fopen(absolutePath, "r");               // Accessing files: https://stackoverflow.com/questions/16869467/command-line-arguments-reading-a-file
             
-            strcpy(command, "grep ");
-            strcat(command, search);
+            strcpy(command, "grep ");                   // grep 
+            strcat(command, search_string);
             strcat(command, " ");
-            strcat(command, filePath);
+            strcat(command, absolutePath);
             strcat(command, " >/dev/null");             // How to redirect stdout to /dev/null: https://unix.stackexchange.com/questions/119648/redirecting-to-dev-null
      
             if(!system(command))                        // System function: https://www.tutorialspoint.com/system-function-in-c-cplusplus#:~:text=The%20system()%20function%20is,%3Cstdlib.
-            {
-                printf("[n] PRESENT %s\n", actualPath);
-            }
+                printf("[%d] PRESENT %s\n", id, absolutePath);
             else
-            {
-                printf("[n] ABSENT %s\n", actualPath);
-            }
+                printf("[%d] ABSENT %s\n", id, absolutePath);
 
             fclose(f);
+            
         }
         else if(strcmp(dp->d_name, "..") != 0 && strcmp(dp->d_name, ".") != 0)  // Check if directory is parent or current: https://stackoverflow.com/questions/50205605/how-to-figure-out-if-the-current-directory-is-the-root-in-c
         {
-            printf("[n] ENQUEUE %s\n", actualPath);
-
             char *nextPath;
             nextPath = (char*)malloc(BUFFER);
             strcpy(nextPath, path);
             strcat(nextPath, "/");
             strcat(nextPath, dp->d_name);
-            grepThread(nextPath, search);
+            count++;
+            enqueue(id, getAbsolutePath(nextPath));
         }
     }
     closedir(dir);
 
-    return NULL;
+    visit(&id);
+
+    // int localCount = count;
+    // while(1)
+    // {
+    //     if(localCount > 0 && currentWorkers < workers)
+    //     {
+    //         id++;
+    //         currentWorkers++;
+    //         pthread_create(&thread[id], NULL, (void *) visit, &id); 
+    //         pthread_join(thread[id], NULL);
+    //         currentWorkers--;
+    //     }
+    // }
+    return 0;
 }
 
 int main(int argc, char *argv[])
 {
-    pthread_t *threads = malloc(sizeof(pthread_t) * atoi(argv[1]));     // Creating n threads: https://stackoverflow.com/questions/20276010/c-creating-n-threads
-    int ret = -1;
+    workers = atoi(argv[1]);
+    search_string = argv[3];
+    
+    // Initialize Threads
+    thread = malloc(sizeof(pthread_t)*workers);
 
-    for(int i = 0; i < atoi(argv[1]); i++)
+    q = (struct taskQueue*)malloc(sizeof(struct taskQueue));
+    struct task* temp = newTask(getAbsolutePath(argv[2]));
+    q->front = q->rear = temp;
+
+    // for(int i = 0; i < atoi(argv[1]); i++)
+    // {
+    //     printf("[%d] Initialized Worker Thread\n", i);
+    // }
+
+    // currentWorkers++;
+
+    // int id = 0;
+    // currentWorkers++;
+    // pthread_create(&thread[id], NULL, (void *) visit, &id);
+    // pthread_join(thread[id], NULL);
+    // currentWorkers--;
+    for(int id = 0; id < workers; id++)
     {
-        pthread_create(&threads[i], NULL, (void *) grepThread, (argv[2], argv[3]));
+        pthread_create(&thread[id], NULL, (void *) visit, &id);
     }
 
-    for(int i = 0; i < atoi(argv[1]); i++)
+    for(int id = 0; id < workers; id++)
     {
-        pthread_join(threads[i], NULL);
+        pthread_join(thread[id], NULL);
     }
+    // visit(0);
 
     return 0;
 }
---
+++ tester.py
@@ -0,0 +1,37 @@
+import requests
+import random
+from pathlib import Path
+
+DEPTH = 3
+FOLDERS = 5
+FILES = 5
+
+word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
+response = requests.get(word_site)
+WORDS = response.content.decode().split("\n")
+
+
+INIT_PATH = "./testdir"
+Path(INIT_PATH).mkdir(parents=True, exist_ok=False)
+
+
+FILENAME = "testcase"
+FOLDERNAME = "testdir_"
+PATH_QUEUE = ["./testdir"]
+
+def create_tests(deep, path):
+    
+    
+    for i in range(FILES):
+        content = [random.choice(WORDS) for i in range(100)]
+
+        with Path('{}/{}{}.txt'.format(path, FILENAME, i)).open("w") as f:
+            f.write("\n".join(content))
+    
+    if deep != 3:
+        for i in range(FOLDERS):
+            new_path = '{}/{}{}'.format(path, FOLDERNAME,i)
+            Path(new_path).mkdir(parents=True, exist_ok=False)
+            create_tests(deep + 1, new_path)
+    
+create_tests(1, INIT_PATH)
