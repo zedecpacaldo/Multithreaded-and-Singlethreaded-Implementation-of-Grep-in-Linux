95916609c859a46db016b435e86dc0d352b0acca
SmurfNiZed <zipacaldo@up.edu.ph>
2023-01-12 18:05:06 +0800

    Multithreading works but no locks, also weird event when thread n outputs (max thread is n-1 only)

--- multithreaded.c
+++ multithreaded.c
@@ -5,15 +5,7 @@
 #include <string.h>
 #include <pthread.h>
 
-#define BUFFER 1000
-
-pthread_mutex_t lock[2];
-pthread_cond_t cond1, cond2, cond3;
-
-int count = 0;
-int workers = 0;
-int currentWorkers = 0;
-char* search_string;
+#define BUFFER 500
 
 // Creating a linked list queue: https://stackoverflow.com/questions/36394860/linked-list-of-strings-in-c
 struct task
@@ -35,29 +27,10 @@ struct task* newTask(char *dir)
     return temp;
 }
 
-char* getAbsolutePath(char* path)
-{
-    char* actualPath;
-    actualPath = (char*)malloc(BUFFER);
-    char *ptr;
-    ptr = realpath(path, actualPath);           // Getting absolute path: https://stackoverflow.com/questions/229012/getting-absolute-path-of-a-file
-
-    return actualPath;
-}
-
 struct taskQueue* q;
+char* search_string;
 pthread_t* thread;
-// struct task* temp = newTask(getAbsolutePath(dir));
-
-
-// struct taskQueue* createQueue(char* dir)
-// {
-//     struct taskQueue* q = (struct taskQueue*)malloc(sizeof(struct taskQueue));
-//     struct task* temp = newTask(getAbsolutePath(dir));
-//     q->front = q->rear = temp;
-    
-//     return q;
-// }
+int workers;
 
 void enqueue(int id, char* dir)
 {
@@ -90,14 +63,14 @@ char* dequeue(int id)
         q->rear = NULL;
     }
 
-    printf("[%d] DIR %s\n", id, getAbsolutePath(dir));
+    printf("[%d] DIR %s\n", id, dir);
     free(temp);
+
     return dir;
 }
 
 void *visit(int* arg)
 {
-    pthread_mutex_lock(&lock[1]);
     int id = *arg;
 
     DIR *dir;
@@ -113,38 +86,35 @@ void *visit(int* arg)
 
     FILE *f;
     char *command;
-
     command = (char*)malloc(BUFFER);
 
     while ((dp = readdir(dir)) != NULL)                 // File
     {
-        char* filePath;
-        filePath = (char*)malloc(BUFFER);
-        
-        strcpy(filePath, path);                         // Concatenating strings: https://www.educative.io/blog/concatenate-string-c
-        strcat(filePath, "/");
-        strcat(filePath, dp->d_name);
-
-        char* absolutePath;
-        absolutePath = getAbsolutePath(filePath);
-
         if (dp->d_type == DT_REG)                       // Checking file types: https://stackoverflow.com/questions/1121383/counting-the-number-of-files-in-a-directory-using-c
-        {                                      
-            f = fopen(absolutePath, "r");               // Accessing files: https://stackoverflow.com/questions/16869467/command-line-arguments-reading-a-file
+        {   
+            char* filePath;
+            filePath = (char*)malloc(BUFFER);
+            
+            strcpy(filePath, path);                         // Concatenating strings: https://www.educative.io/blog/concatenate-string-c
+            strcat(filePath, "/");
+            strcat(filePath, dp->d_name);   
+
+            f = fopen(filePath, "r");               // Accessing files: https://stackoverflow.com/questions/16869467/command-line-arguments-reading-a-file
             
             strcpy(command, "grep ");                   // grep 
             strcat(command, search_string);
             strcat(command, " ");
-            strcat(command, absolutePath);
+            strcat(command, filePath);
             strcat(command, " >/dev/null");             // How to redirect stdout to /dev/null: https://unix.stackexchange.com/questions/119648/redirecting-to-dev-null
      
             if(!system(command))                        // System function: https://www.tutorialspoint.com/system-function-in-c-cplusplus#:~:text=The%20system()%20function%20is,%3Cstdlib.
-                printf("[%d] PRESENT %s\n", id, absolutePath);
+                printf("[%d] PRESENT %s\n", id, filePath);
             else
-                printf("[%d] ABSENT %s\n", id, absolutePath);
+                printf("[%d] ABSENT %s\n", id, filePath);
 
             fclose(f);
-            pthread_cond_signal(&cond1);
+
+            free(filePath);
             
         }
         else if(strcmp(dp->d_name, "..") != 0 && strcmp(dp->d_name, ".") != 0)  // Check if directory is parent or current: https://stackoverflow.com/questions/50205605/how-to-figure-out-if-the-current-directory-is-the-root-in-c
@@ -154,36 +124,20 @@ void *visit(int* arg)
             strcpy(nextPath, path);
             strcat(nextPath, "/");
             strcat(nextPath, dp->d_name);
-            enqueue(id, getAbsolutePath(nextPath));
+
+            enqueue(id, nextPath);
         }
     }
     closedir(dir);
-
-    if(currentWorkers > workers)
-    {
-        pthread_cond_wait(&cond1, &lock[1]);
-    }
+    free(command);
+    free(path);
     visit(&id);
-    pthread_mutex_unlock(&lock[1]);
-    
-    // int localCount = count;
-    // while(1)
-    // {
-    //     if(localCount > 0 && currentWorkers < workers)
-    //     {
-    //         id++;
-    //         currentWorkers++;
-    //         pthread_create(&thread[id], NULL, (void *) visit, &id); 
-    //         pthread_join(thread[id], NULL);
-    //         currentWorkers--;
-    //     }
-    // }
+
     return 0;
 }
 
 int main(int argc, char *argv[])
 {
-    pthread_mutex_lock(&lock[0]);
     workers = atoi(argv[1]);
     search_string = argv[3];
     
@@ -191,27 +145,17 @@ int main(int argc, char *argv[])
     thread = malloc(sizeof(pthread_t)*workers);
 
     q = (struct taskQueue*)malloc(sizeof(struct taskQueue));
-    struct task* temp = newTask(getAbsolutePath(argv[2]));
+
+    char* absolutePath;
+    absolutePath = (char*)malloc(BUFFER);
+    realpath(argv[2], absolutePath);
+    struct task* temp = newTask(absolutePath);
+
     q->front = q->rear = temp;
 
-    // for(int i = 0; i < atoi(argv[1]); i++)
-    // {
-    //     printf("[%d] Initialized Worker Thread\n", i);
-    // }
-
-    // currentWorkers++;
-
-    // int id = 0;
-    // currentWorkers++;
-    // pthread_create(&thread[id], NULL, (void *) visit, &id);
-    // pthread_join(thread[id], NULL);
-    // currentWorkers--;
     for(int id = 0; id < workers; id++)
     {
-        if(currentWorkers > workers)
-        {
-            pthread_cond_wait(&cond1, &lock[0]);
-        }
+        printf("Creating worker %d...\n", id);
         pthread_create(&thread[id], NULL, (void *) visit, &id);
     }
 
@@ -219,7 +163,9 @@ int main(int argc, char *argv[])
     {
         pthread_join(thread[id], NULL);
     }
-    // visit(0);
-    pthread_mutex_unlock(&lock[0]);
+
+    free(q);
+    free(thread);
+
     return 0;
 }
